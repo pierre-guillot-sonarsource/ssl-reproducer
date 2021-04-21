74f535e4db2268ea50b85fcddbd7c23b71cc323f
Pierre <pierre.guillot@sonarsource.com>
2021-04-21 12:36:50 +0200

    init commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.io.IOException;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class App {
+  public static void main(String[] args) {
+    OkHttpClient client = new OkHttpClient.Builder().build();
+
+    Request request = new Request.Builder()
+      .method("GET", null)
+      .url("https://sonarsource-bitbucket-test.valiantys.net/rest/api/1.0/repos")
+      .build();
+
+    try (Response response = client.newCall(request).execute()) {
+      System.out.println(response.code());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
